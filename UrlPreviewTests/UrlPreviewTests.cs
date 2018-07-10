470336e7778d88f7eea53faec17d9b69daac8461
Kris Siegel <krissiegel@gmail.com>
2018-07-09 23:39:59 -0700

    Move to xunit instead

--- UrlPreviewTests/UrlPreviewTests.cs
+++ UrlPreviewTests/UrlPreviewTests.cs
@@ -1,18 +1,30 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 using System.Threading.Tasks;
 
 namespace BinaryIdiot
 {
-    [TestClass]
     public class UrlPreviewTests
     {
-        [TestMethod]
-        public async Task FetchAndParseCNNFromUrl()
+        [Fact]
+        public void FetchAndParseCNNFromUrl()
         {
-            UrlPreviewModel result = await UrlPreview.FetchPreviewFromUrlAsync("https://www.cnn.com/");
-            Assert.IsNotNull(result);
-            Assert.IsNotNull(result.Title);
-            Assert.IsNotNull(result.Description);
+            var task = UrlPreview.FetchPreviewFromUrlAsync("https://www.cnn.com");
+            task.Wait();
+            var result = task.Result;
+            Assert.NotNull(result);
+            Assert.NotNull(result.Title);
+            Assert.NotNull(result.Description);
+        }
+
+        [Fact]
+        public void FetchAndParseBloombergFromUrl()
+        {
+            var task = UrlPreview.FetchPreviewFromUrlAsync("https://www.bloomberg.com/news/articles/2018-07-04/poland-s-isolation-shown-as-premier-is-slammed-in-eu-parliament");
+            task.Wait();
+            var result = task.Result;
+            Assert.NotNull(result);
+            //Assert.NotNull(result.Title);
+            //Assert.NotNull(result.Description);
         }
     }
 }
